de34c886cdb5ca373e7061cda0af59b87fb78c4d
Your Name <you@example.com>
2026-06-11 16:48:22 +0200

    add initial migration and model for ImagePost

--- feed/models.py
+++ feed/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class ImagePost(models.Model):
+    description = models.TextField(null=True, blank=True)
+    likes = models.IntegerField(default=0)
+    image = models.ImageField(upload_to="images/")
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
