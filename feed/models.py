f6518b779e588654bf70566571238875e6212f00
Your Name <you@example.com>
2026-06-11 17:45:21 +0200

    add static files configuration and update ImagePost model for image uploads

--- feed/models.py
+++ feed/models.py
@@ -5,6 +5,6 @@ from django.db import models
 class ImagePost(models.Model):
     description = models.TextField(null=True, blank=True)
     likes = models.IntegerField(default=0)
-    image = models.ImageField(upload_to="images/")
+    image = models.ImageField(upload_to="static/images/")
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
